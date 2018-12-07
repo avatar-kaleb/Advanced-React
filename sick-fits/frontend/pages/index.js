22cf7ab7e183b7ab35614bcdbbe7c95fc38e8ad5
Kaleb McKelvey <kalebmckelvey@northwesternmutual.com>
2018-12-07 16:52:47 -0500

    updated components up to module 3

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = (props) => {
+  return (
+    <div>
+      <p>
+        Hey!
+      </p>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sell = () => {
+  return (
+    <div>
+      <p>
+        Sell
+      </p>
+    </div>
+  )
+}
+
+export default Sell
