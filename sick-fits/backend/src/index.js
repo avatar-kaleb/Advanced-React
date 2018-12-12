0e574a83e85103abba5fdd039a5bbab79f48bc60
Kaleb McKelvey <kmckelvey3@outlook.com>
2018-12-12 09:08:26 -0500

    updated to module 5

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+server.express.use(cookieParser());
+
+// TODO use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server running on port http://localhost:${deets.port}`);
+  }
+);
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,68 @@
-const mutations = {};
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-module.exports = mutations;
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // take copy of updates
+    const updates = { ...args };
+
+    // remove id
+    delete updates.id;
+
+    // run update
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // check if they own or have permissions
+    // TODO
+    // delete
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    // hash password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+
+    // create jwt token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return user;
+  }
+};
+
+module.exports = Mutation;
